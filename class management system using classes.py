14f89bfeb5a2c7fe382d425fd0db86dcd1181c6d
kuldeepsinghvi <kuldeepsinghvi7@gmail.com>
2026-06-14 22:02:03 +0530

    Class management using class

--- class management system using classes.py
+++ class management system using classes.py
@@ -1,7 +1,7 @@
 class Student_Manager():
     
     #check if roll no exist already or not before adding it    
-    def set_roll_number(self,student_db,roll_no):
+    def check_roll_number(self,student_db,roll_no):
         if(roll_no in student_db.keys()):
             print("Roll no already exist")
             return 1
@@ -13,15 +13,18 @@ class Student_Manager():
         student_db[roll_no]=f_name
 
     #update the student details in db
-    def update_student(self,student_db,roll_no,f_name,l_name):
-        student_db[roll_no][1]=f_name
-        student_db[roll_no][2]=l_name
+    def update_student(self,student_db,roll_no,roll_no_new,f_name,l_name):
+        f = student_db[roll_no]
+        del student_db[roll_no]
+        f.student_update_data(f_name,l_name,roll_no_new)
+        student_db[roll_no_new]=f
+
         print(f"{roll_no} data have been updated")
 
     #delete the student details in db after checking if roll_no exist or not
     def Delete_student(self, student_db, roll_no):
         if(roll_no in student_db):
-            student_db[roll_no].pop()
+            del student_db[roll_no]
             print(f"roll no - {roll_no} Data Has Been Deleted")
         else:
             print(f"roll no - {roll_no} Doesn't Exist !")
@@ -29,24 +32,34 @@ class Student_Manager():
     #search for the student data in db using roll no
     def search_student(self, student_db ,roll_no):
         if(roll_no in student_db):
-            print(f"roll no - {roll_no}" , f"First Name - {student_db[roll_no][0]}", f"Last Name - {student_db[roll_no][1]}" , sep="\n" )
+            f = student_db[roll_no]
+            f.student_data()
         else:
             print(f"{roll_no} Doesn't Exist !")
 
     #print all the student data in the db 
-    def all_students(self, student_db):
-        print("Here is the list of all student - ")
-        for i in student_db.keys():
-            print(f" Roll no - {i}")
-            print(f"First Name - {student_db[i][0]}")
-            print(f"Last Name - {student_db[i][1]}")
+    def P_students(self, student_db):
+         print("Here is the list of all student - ")
+         for i in student_db.keys():
+            f = student_db[i]
+            f.student_data()
 
 
 class Student:
     def __init__(self,roll_no,f_name,l_name):
              self.roll_no = roll_no
-             self.roll_no = f_name
+             self.f_name = f_name
              self.l_name = l_name
+    
+    def student_data(self):
+        print(f"Roll no - ", self.roll_no)
+        print(f"First Name - ", self.f_name)
+        print(f"Last Name - " ,self.l_name)
+
+    def student_update_data(self,f_name,l_name,roll_no):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.roll_no = roll_no
 
 choice = 0 
 student_db = {}
@@ -57,30 +70,34 @@ while( choice != 6):
     
     if(choice == 1):
         roll_no = int(input("Enter the roll no - "))
-        f = s1.set_roll_number(student_db,roll_no)
+        f = s1.check_roll_number(student_db,roll_no)
         if(f== 0):
             f_name , l_name = input("Enter First name and last name - ").split()
             f_name = Student(roll_no,f_name,l_name) 
-            s1=s1.add_student(student_db,roll_no,f_name)
+            s1.add_student(student_db,roll_no,f_name)
         else:
             pass
 
     elif(choice == 2):
         roll_no = int(input("Enter the roll no whose data needs to be updated - "))
-        f_name , l_name = input("Enter First name and last name to be updated- ").split()
-        roll_no_new = int(input("Enter the roll no - "))
-        s1=s1.update_student(student_db,roll_no,roll_no_new,f_name,l_name)
+        f = s1.check_roll_number(student_db,roll_no)
+        if(f== 1):
+            f_name , l_name = input("Enter First name and last name to be updated- ").split()
+            roll_no_new = int(input("Enter the new roll no - "))
+            s1.update_student(student_db,roll_no,roll_no_new,f_name,l_name)
+        else:
+            print(f"{roll_no} doesnt exist !")
 
     elif(choice == 3):
         roll_no = int(input("Enter the roll no whose data needs to be Deleted - "))
-        s1=s1.Delete_student(student_db,roll_no)
+        s1.Delete_student(student_db,roll_no)
 
     elif(choice == 4):
          roll_no = int(input("Enter the roll no whose data needs to be Searched - "))
          s1.search_student(student_db,roll_no)
     
     elif(choice == 5):
-        s1.all_students(student_db)
+        s1.P_students(student_db)
     
     else:
         exit()
